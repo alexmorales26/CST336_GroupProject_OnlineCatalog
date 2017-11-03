82097bf067899d6057da3009df8a085ce1e3708c
Alfonso T <alftorres@csumb.edu>
2017-11-03 19:22:50 +0000

    Adding Database Connection

---
+++ dbCon.php
@@ -0,0 +1,18 @@
+<?php
+    function getDatabaseConnection(){
+    
+    $host = 'us-cdbr-iron-east-05.cleardb.net';//Heroku
+    $dbname = 'heroku_5cc899d91ebdb63';
+    $username = 'bc84338c545651';
+    $password = '06d1624f';
+    
+    //creates db connection
+    $dbConn = new PDO("mysql:host=$host; dbname=$dbname",$username, $password);
+    
+    //display errors when accessing tables
+    $dbConn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    return $dbConn;
+    
+}
+
+?>
