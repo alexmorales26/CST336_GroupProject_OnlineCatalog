a0f4f8951edd8501677cdb70fc9538d178d578de
Bret Stine <bstine@csumb.edu>
2017-11-05 23:47:55 +0000

    adding things

---
+++ Popularity.php
@@ -0,0 +1,90 @@
+<?php
+    include 'dbCon.php';
+    $conn = getDatabaseConnection();
+    include 'inc/function.php';
+    
+    function displayMovies() {
+        global $conn;
+        $sql = "SELECT * FROM `db_movie`";
+                
+        $statement = $conn->prepare($sql);
+        $statement->execute();
+        $movies = $statement->fetchAll(PDO::FETCH_ASSOC);
+        //This will return an array of movie info
+        return $movies;
+    }
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
+    <title> MovieNator</title>
+    <!--
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+    -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+  </head>
+  <body>
+    <h1> MovieNator </h1>
+        
+        <nav class="navbar navbar-inverse navbar-static-top">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#">Movie Database</a>
+                </div>
+                <div class="collapse navbar-collapse" id="myNavbar">
+                    <ul class="nav navbar-nav">
+                        <li class="active"><a href="#">Home</a></li>
+                        <li><a href="Popularity.php">Popular</a></li>
+                        <li><a href="#">Genre</a></li>
+                        <li class="dropdown">
+                            <a class="dropdown-toggle" data-toggle="dropdown" href="#">More
+                            <span class="caret"></span></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="#">Crime</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#">Checkout</a></li>
+                        <li><a href="#">Random</a></li>
+                        <li class="dropdown">
+                            <a class="dropdown-toggle" data-toggle="dropdown" href="#">More
+                            <span class="caret"></span></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="#">Length</a></li>
+                                <li><a href="#">A-Z</a></li>
+                                <li><a href="#">Z-A</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                    <form class="navbar-form navbar-right">
+                        <div class="input-group">
+                            <input type="text" class="form-control" placeholder="Search">
+                            <div class="input-group-btn">
+                                <button class="btn btn-default">
+                                    <i class="glyphicon glyphicon-search"></i>
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </nav>
+
+    <?php
+    
+      $trends=trending();
+      foreach($trends as $trend)
+      {
+          echo "<img src='$trend' width='200'>";
+          echo "<br>" . "<br>";
+        
+      }
+    ?>
+  </body>
+</html>
